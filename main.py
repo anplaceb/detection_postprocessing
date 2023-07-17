bc9734fc7c9efd6f6df7e70b6b0a359c4cf937d2
anplaceb <anplaceb@gmail.com>
2023-07-17 11:27:06 +0200

    Commit main

---
+++ main.py
@@ -0,0 +1,21 @@
+import area_filter_raster
+
+def main(name):
+    # apply forest mask
+
+    # closing and opening
+
+    # area filter
+    area_filter_raster.fun_area_filter_raster()
+    # raster to polygon
+
+    # agri area
+
+    # if local mlocal spatial overlapping
+
+    # difference previous
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
