35f566ab888d594d592823dfb6a187d23d07aaa6
anplaceb <anplaceb@gmail.com>
2023-07-21 13:13:10 +0200

    postprocessing all steps

--- difference_previous_detection.py
+++ difference_previous_detection.py
@@ -11,8 +11,6 @@ def diff_prev_detection(polygon, output, past_files_path, first_year):
     # List only polygons from previous years as the actual year
     year = re.findall(r"(?<!\d)\d{4}(?!\d)", polygon)[0]  # search for current year for later searching for past
     # detection
-    print(year, type(year))
-    print(first_year, type(first_year))
     if year != first_year:
         arcpy.PairwiseErase_analysis(in_features=polygon,
                                      erase_features=os.path.join(past_files_path, f'past_detection_{year}'),
